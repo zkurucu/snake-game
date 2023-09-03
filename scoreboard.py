42e09b94d8baf7bbefdbf3f7f29e894a1a86b59d
zkurucu <z.kurucu.d@hotmail.com>
2023-09-03 14:10:05 +0300

    add a highest score to the snake game

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highest_score = 0
         self.penup()
         self.color("white")
         self.goto(0, 270)
@@ -14,11 +15,14 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score:{self.score}", font=FONT, align=ALIGNMENT)
+        self.clear()
+        self.write(f"Score:{self.score}   Highest Score:{self.highest_score}", font=FONT, align=ALIGNMENT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align= ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.highest_score:
+            self.highest_score = self.score
+        self.score = 0
+        self.update_score()
 
     def increase_score(self):
         self.clear()
